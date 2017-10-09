69f27c6bb5fbb97440a350de547935f5e43e630b
nasser-ahmad <info.nasser.88@gmail.com>
2017-10-09 14:47:04 +0300

    edit session_controller redirect to root_url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	user = User.find_by(name: params[:session][:name].downcase)
   	if user && user.authenticate(params[:session][:password])
   		log_in user
-  		redirect_to user
+  		redirect_to root_url
   	else
   		flash.now[:danger] = 'اسم المستخدم غير متوافق مع كلمة المرورو!! تأمد من الادخال ..'
   		render 'new'
